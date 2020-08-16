30b19e48fba4ed35ef3f46767fd22441d35ae330
Mateusz Bieliñski <mateusz100022@gmail.com>
2020-08-16 18:31:55 +0200

    prepare to build

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+
+        clean: {
+            dist: "dist/"
+        },
+
+        targethtml: {
+            dist: {
+                files: {
+                    "dist/public/index.html": "public/index.html"
+                }
+            }
+        },
+
+        concat: {
+            dist: {
+                files: {
+                    'dist/public/js/libs.min.js': [
+                        "public/js/libs/jquery.js",
+                        "public/js/libs/underscore.js",
+                        "public/js/libs/backbone.js",
+                        "public/js/libs/*.js"
+                    ],
+                    "dist/public/js/app.min.js": [
+                        "public/js/App.js",
+                        "public/js/models/*.js",
+                        "public/js/collections/*.js",
+                        "public/js/views/*.js",
+                        "public/js/routers/*.js"
+                    ]
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "dist/public/js/",
+                        src: "*.js",
+                        dest: "dist/public/js/"
+                    }
+                ]
+            }
+        },
+        cssmin: {
+            dist: {
+                files: {
+                    'dist/public/css/style.min.css': "public/css/style.css"
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true
+                },
+                files: {
+                    "dist/public/index.html": "dist/public/index.html"
+                }
+            }
+        },
+        copy: {
+            dist: {
+                files: [ 
+                    {expand: true, src: 'server.js', dest: "dist/"},
+                    {expand: true, cwd: "public/images/", src: "*", dest: "dist/public/images/"}
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-targethtml');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build', ['clean', 'targethtml', 'concat', 'uglify', 'cssmin', 'htmlmin', 'copy']);
+
+}
